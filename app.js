edebaee75521ee15826519c043483d896a87ccb3
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2023-06-11 11:14:53 -0300

    feat: in app.js implemented playerSelection function that returns the the choice from the user

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ function getComputerChoice(){
     const randNumber = Math.floor((Math.random() * 3)) 
     return arr[randNumber] 
 } 
+
+function playerSelection(){
+    const arr = ["Rock", "Paper", "Scissors"] 
+    let answerNumber;
+
+    while(true){
+        answerNumber = parseInt(prompt("Choose: \n[1] Rock \n[2] Paper \n[3] Scissors"))
+        if(answerNumber >= 1 && answerNumber <= 3)
+            break
+    }
+    return arr[answerNumber - 1]
+}
     
-    alert(getComputerChoice())
+    alert(playerSelection())
